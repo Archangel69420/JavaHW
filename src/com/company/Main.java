fb2653b8428f2663cf30a386c8a29d296393f12c
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2021-09-08 16:47:12 +0300

    ARRAYSHW1

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] hwArray = {2, 5, -3, 11, 193};
+        for (int j : hwArray) {
+            System.out.println(j);
+        }
+    }
+}

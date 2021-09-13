509d729c43376ba7d98817248fccad4fedcd93d0
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2021-09-13 16:32:51 +0300

    Prime number question. Note: Use command line arguements ONLY.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int m = 1;
-        int[] array = new int[args.length];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(args[i]);
+        PrimeFind cap = new PrimeFind(70);
+        System.out.println(cap.controller());
+    }
+}
+
+class PrimeFind {
+    int primeCap;
+
+    public PrimeFind (int input) {
+        primeCap = input;
+    }
+
+    public String controller() {
+        return allPrimes(primeCap);
+    }
+
+    private String allPrimes(int input) {
+        if (input == 2) return "2";
+        if (isPrime(input)) return Integer.toString(input) + " "
+                + allPrimes(input-1);
+        return allPrimes(input - 1);
+    }
+
+    private boolean isPrime (int primetest) {
+        boolean isPrime = true;
+        for (int i = 2; i < Math.sqrt(primetest); i = i + 2) {
+            if (primetest % i == 0) {
+                isPrime = false;
+                break;
+            }
         }
-        int[][] bigger_smaller = new int[2][array.length];
-        for (int i = 0; i < args.length; i++) {
-            if (array[i] > m) bigger_smaller[0][i] = array[i] - m;
-            if (array[i] < m) bigger_smaller[1][i] = array[i] - m;
-        }
-        System.out.print("Bigger: ");
-        for (int v : bigger_smaller[0]) {
-            if (v != 0)
-                System.out.print(v + m + " ");
-        }
-        System.out.println("\n ----");
-        System.out.print("Smaller: ");
-        for (int z : bigger_smaller[1]) {
-            if (z != 0) System.out.print(z + m + " ");
-        }
+        return isPrime;
     }
 }

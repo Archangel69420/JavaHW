73f16a8601d5e5c86ecc721a2d169d1b574ad89d
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2021-09-22 11:40:22 +0300

    AllFactors Program - Rev 2.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,44 +2,70 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int[] integers = {2, 4, 6, 8, 3, 5, 7, 8,
-                3, 7, 76, 5, 454, 63, 6, 3, 645, 6, 4,
-                6, 3546, 34, 6, 36, 3, 6};
-        arrayPrinter(controller(integers));
+        int[] integers = {4, 532, 45, 23, 5324, 5, 23, 52, 5, 423, 54, 5, 234, 5234, 5, 2352, 35, 2};
+        AllFactors some = new AllFactors(integers.length);
+        some.setArray(integers);
+        some.printArray();
+        some.printSolution();
+    }
+}
+
+class AllFactors {
+    private int length;
+
+    public AllFactors(int input) {
+        length = input;
     }
 
-    static String[] controller(int[] someArray) {
-        String[] allFactors = new String[someArray.length];
-        for (int i = 0; i < someArray.length; i++) {
-            allFactors[i] = factorsOf(someArray[i]);
+    private int[] array = new int[length];
+
+    public void setArray(int[] someArray) {
+        array = someArray;
+    }
+
+    public String[] controller() {
+        String[] allFactors = new String[length];
+        for (int i = 0; i < length; i++) {
+            allFactors[i] = factorsOf(array[i]);
         }
         return allFactors;
     }
 
-    static String factorsOf(int num) {
+    private String factorsOf(int num) {
         if (isPrime(num)) return toString(num);
         int quotient = oneFactor(num);
         return toString(quotient) + factorsOf(num / quotient);
     }
 
-    static boolean isPrime(int input) {
+    private boolean isPrime(int input) {
         for (int i = 2; i <= Math.sqrt(input); i++) {
             if (input % i == 0) return false;
         }
         return true;
     }
 
-    static String toString(int n) {
+    private String toString(int n) {
         return n + " ";
     }
 
-    static void arrayPrinter(String[] givenArray) {
-        for (String v : givenArray) {
-            System.out.println(v);
+    public void printArray() {
+        System.out.println("The given array is: ");
+        for (int v : array) {
+            System.out.println(v + " ");
         }
+        System.out.println("--------------------------");
     }
 
-    static int oneFactor(int num) {
+    public void printSolution() {
+        String[] solutions = controller();
+        System.out.println("The solutions are: ");
+        for (String v : solutions) {
+            System.out.println(v);
+        }
+        System.out.println("--------------------------");
+    }
+
+    private int oneFactor(int num) {
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return i;

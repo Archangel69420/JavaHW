7bad6b75105874503636639533dc1fa4af6e5c01
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2021-09-29 11:37:37 +0300

    Homework: Using Classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,31 +1,52 @@
 package com.company;
-
-public class Main {
+import static ibio.Helpers.*;
+/* public class Main {
     public static void main(String[] args) {
-        int[] test = {1,2,3,4,5,6,7,8,9};
-        int[] result = alternate(test);
-        for (int v : result) {
-            System.out.print(v + " ");
+        for (int k = 0; k <= 10; k++) {
+            double a = k * Math.PI / 5;
+            System.out.print(a + ", ");
+            System.out.print(Math.sin(a) + ", ");
+            System.out.print(Math.cos(k * Math.PI / 5));
+            System.out.print("\n");
         }
     }
+} */
 
-    static int[] alternate (int[] input) {
-        int[] result = new int[input.length];
-        boolean counter = true;
-        int startInd = 0, endInd = input.length - 1;
-        /* while (startInd < input.length) {
-            if (counter) {
-                result[startInd] = input[startInd/2];
-                startInd++;
-                counter = false;
-            }
-            else {
-                result[startInd] = input[endInd];
-                endInd--;
-                counter = true;
-                startInd++;
-            }
-        } */
-        return result;
+/* public class Main {
+    public static void main(String[] args) {
+        calculator();
     }
-}
+
+    static void calculator() {
+        boolean running = true;
+        String operation;
+        int a, b;
+        while (running) {
+            operation = input("give operation: ");
+            System.out.println(operation);
+            if (operation.equals("end")) {running = false;}
+            else {
+                a = inputInt("give first integer operand: ");
+                b = inputInt("give second integer operand: ");
+                if (operation.equals("add")) output(a + b);
+                else {
+                    if (operation.equals("sub")) output(a - b);
+                    else output("unknown operation");
+                }
+            }
+        }
+    }
+} */
+
+/* public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Not enough arguements detected");
+            System.exit(1);
+        }
+        String[] results = args[0].split(args[1]);
+        for (String v: results) {
+            if (v.length() > 0) System.out.println(v);
+        }
+    }
+} */

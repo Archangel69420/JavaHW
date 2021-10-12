ac5a21db3cae805919b66bc80a6eadaa726a9f32
Archangel69420 <89769725+Archangel69420@users.noreply.github.com>
2021-10-12 13:02:21 +0300

    Homework: Q5 or 6 I honestly donno adding numbers in a file together.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,18 @@
 package com.company;
-import static ibio.Helpers.*;
- public class Main {
-    public static void main(String[] args) {
-        for (int k = 0; k <= 10; k++) {
-            double a = k * Math.PI / 5;
-            System.out.print(a + ", ");
-            System.out.print(Math.sin(a) + ", ");
-            System.out.print(Math.cos(a));
-            System.out.print("\n");
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        try (Scanner in = new Scanner(new File("text.txt"))){
+            long sum = 0;
+            while (in.hasNextInt()) {
+                sum += in.nextInt();
+            }
+            System.out.println(sum);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
-
-/* public class Main {
-    public static void main(String[] args) {
-        calculator();
-    }
-
-    static void calculator() {
-        boolean running = true;
-        String operation;
-        int a, b;
-        while (running) {
-            operation = input("give operation: ");
-            System.out.println(operation);
-            if (operation.equals("end")) {running = false;}
-            else {
-                a = inputInt("give first integer operand: ");
-                b = inputInt("give second integer operand: ");
-                if (operation.equals("add")) output(a + b);
-                else {
-                    if (operation.equals("sub")) output(a - b);
-                    else output("unknown operation");
-                }
-            }
-        }
-    }
-} */
-
-/* public class Main {
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Not enough arguements detected");
-            System.exit(1);
-        }
-        String[] results = args[0].split(args[1]);
-        for (String v: results) {
-            if (v.length() > 0) System.out.println(v);
-        }
-    }
-} */
